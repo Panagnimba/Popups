ae617420bc407cbf8f14f27e552adea498dbb4b3
Panagnimba <panagnimba@gmail.com>
2022-07-07 20:16:57 +0200

    up

--- pages/Login/index.vue
+++ pages/Login/index.vue
@@ -54,7 +54,7 @@ export default {
       //  EFFACER LES CHAMPS INPUTS
       (this.username = ""), (this.password = "");
       //   SEND THE REQUEST TO LOGIN
-      let response = await this.$axios.post("/login", user);
+      let response = await this.$axios.post(`${process.env.HOST}/login`, user);
       if (response.status == 200 && response.data.token != null) {
         // AUTHENTICATE USER IN THE STORE
         this.$store.commit("setAuthUser", response.data);
